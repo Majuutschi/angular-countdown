b7193047e85b71b06287bf12da0c4afdb37b164c
Maja Garpelin <Majagarpelin@gmail.com>
2022-09-06 15:27:39 +0200

    date changes

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -14,9 +14,9 @@ export class TestComponent implements OnInit {
   public dateInput: string = '2022-09-06';
 
   public days: number = 0;
-  public hours: number = 0;
-  public minutes: number = 0;
-  public seconds: number = 0;
+  public hours: any = 0;
+  public minutes: any = 0;
+  public seconds: any = 0;
 
   currentDate: any;
   currentInMilliseconds: any;
@@ -38,37 +38,59 @@ export class TestComponent implements OnInit {
 
   calculateTitleWidth(length: any) {
     console.log(this.innerWidth)
-    return (this.innerWidth * 1.7)/(length+8) +'px'
+    return (this.innerWidth * 1.7)/(length + 8) +'px'
   }
 
   calculateDateWidth(length: any) {
     if(this.dateInput.length <= 6) {
-      return (this.innerWidth * 2.6)/(length+22) +'px'
+      console.log(length)
+      return (this.innerWidth * 2.4)/(length + 24) +'px'
     } else {
-      return (this.innerWidth * 2.6)/(length+14) +'px'
+      console.log(this.days)
+      if(this.days >= 1000) {
+        return (this.innerWidth * 1.9)/(length + 14) +'px'
+      } else if(this.days >= 100) {
+        return (this.innerWidth * 2.0)/(length + 14) +'px'
+      } else if (this.days >=10) {
+        return (this.innerWidth * 2.1)/(length + 14) +'px'
+      } else {
+        return (this.innerWidth * 2.4)/(length + 14) +'px'
+      }
+      
     }
   }
 
 
   myCountdown() {
-    this.currentDate = new Date();
-    this.selectedDate = new Date(this.dateInput);
-    this.currentInMilliseconds = this.currentDate.getTime();
-    this.selectedInMilliseconds = this.selectedDate.getTime();
-    this.difference = this.selectedInMilliseconds - this.currentInMilliseconds;
-    this.seconds = Math.floor(this.difference / 1000);
-    this.minutes = Math.floor(this.seconds / 60);
-    this.hours = Math.floor(this.minutes / 60);
-    this.days = Math.floor(this.hours / 24);
-  }
-  
-  handleDate() {
-    if (this.dateInput.length <= 6) {
-      console.log("hello")
+    if(this.dateInput.length > 6) {
+      this.currentDate = new Date();
+      this.selectedDate = new Date(this.dateInput);
+      this.currentInMilliseconds = this.currentDate.getTime();
+      this.selectedInMilliseconds = this.selectedDate.getTime();
+      this.difference = this.selectedInMilliseconds - this.currentInMilliseconds;
+      
+      this.seconds = Math.floor(this.difference / 1000);
+      this.minutes = Math.floor(this.seconds / 60);
+      this.hours = Math.floor(this.minutes / 60);
+      this.days = Math.floor(this.hours / 24);
+
+      this.hours %= 24;
+      this.minutes %= 60;
+      this.seconds %= 60;
+      this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
+      this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+      this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+
+      
+
+    } else {
+      this.days = 0;
+      this.hours = 0;
+      this.minutes = 0;
+      this.seconds = 0;
     }
-    else {
-      console.log("bye")
-    }
+
+    setInterval(this.myCountdown, 1000);
     
   }
   
