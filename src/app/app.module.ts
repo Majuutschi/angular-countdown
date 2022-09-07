32d9bed211ec947c2090b13a4683dfd27961177d
Maja Garpelin <Majagarpelin@gmail.com>
2022-09-07 15:37:05 +0200

    heroku

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 
@@ -13,7 +12,6 @@ import { TestComponent } from './components/test/test.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     FormsModule
   ],
   providers: [],
