73b40e2e0e9f821e584d6cfb7ea80d4cff364359
Maja Garpelin <Majagarpelin@gmail.com>
2022-09-07 14:12:38 +0200

    styling

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -65,32 +65,40 @@ export class TestComponent implements OnInit {
 
   startCountdown() {
     console.log('start')
-    if(this.dateInput.length > 6) {
-      if (this.dateInput >= this.currentDate) {
+    if(this.dateInput.length > 9) {
+      console.log(this.dateInput)
+      console.log(this.currentDate)
+      
+      if (this.dateInput > this.currentDate) {
+        if(this.dateInput == 'åååå-mm-dd') {
+          this.days = 0;
+          this.hours = 0;
+          this.minutes = 0;
+          this.seconds = 0;
+        } else {
+          setInterval(() => {
+            this.currentDate = new Date();
+            this.selectedDate = new Date(this.dateInput);
+            this.currentInMilliseconds = this.currentDate.getTime();
+            this.selectedInMilliseconds = this.selectedDate.getTime();
+            this.difference = this.selectedInMilliseconds - this.currentInMilliseconds;
+            
+            this.seconds = Math.floor(this.difference / 1000);
+            this.minutes = Math.floor(this.seconds / 60);
+            this.hours = Math.floor(this.minutes / 60);
+            this.days = Math.floor(this.hours / 24);
         
-        setInterval(() => {
-          this.currentDate = new Date();
-          this.selectedDate = new Date(this.dateInput);
-          this.currentInMilliseconds = this.currentDate.getTime();
-          this.selectedInMilliseconds = this.selectedDate.getTime();
-          this.difference = this.selectedInMilliseconds - this.currentInMilliseconds;
-          
-          this.seconds = Math.floor(this.difference / 1000);
-          this.minutes = Math.floor(this.seconds / 60);
-          this.hours = Math.floor(this.minutes / 60);
-          this.days = Math.floor(this.hours / 24);
-      
-          this.hours %= 24;
-          this.minutes %= 60;
-          this.seconds %= 60;
-          this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
-          this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
-          this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
-        }, 1000)
+            this.hours %= 24;
+            this.minutes %= 60;
+            this.seconds %= 60;
+            this.hours = this.hours < 10 ? '0' + this.hours : this.hours;
+            this.minutes = this.minutes < 10 ? '0' + this.minutes : this.minutes;
+            this.seconds = this.seconds < 10 ? '0' + this.seconds : this.seconds;
+          }, 1000)
+        }
       }
-      
-      
     } else {
+      
       this.days = 0;
       this.hours = 0;
       this.minutes = 0;
